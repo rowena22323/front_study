0c0b548d494cc7179bbf93a8f3ffc6bd4b8bef86
rowena22323 <53651175+rowena22323@users.noreply.github.com>
2020-04-27 15:57:47 +0900

    2020-04-27 web_4 javascript
    attribution of CSS
    basic javascript

---
+++ javascript/jsVariable.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<script type="text/javascript">
+		var i = 0;
+		document.write(i);
+		document.write();
+		var name = "홍길동"
+		document.write(name);
+		document.write(<br>);
+		var a = 1;
+		var b = 2;
+		var c = a + b;
+		document.write(c);
+
+	</script>
+	
+</head>
+<body>
+
+</body>
+</html>
